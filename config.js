16ad2a5c5c57eb4d7ba5545faff06879b4ae05db
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2023-12-20 10:49:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94782543893', 'Hashan', true],
+  ['94771040589', 'Hashan', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['94782543893','94782543893'] 
+global.prems = ['94782543893', '94782543893', '94782543893']
+global.allowed = ['94782543893', '94782543893', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,17 +50,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '0000'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.packname = 'Hashan' 
+global.author = '@Hashan' 
+global.menuvid = 'https://telegra.ph//file/fff37bbd3114a90f4a8fe.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/hashan._.sampath\n' 
+global.dygp = 'https://wa.me/qr/P6WADRJOKAFUK1'
+global.fgsc = 'https://wa.me/qr/P6WADRJOKAFUK1' 
+global.fgyt = 'https://wa.me/qr/P6WADRJOKAFUK1'
+global.fgpyp = 'https://wa.me/qr/P6WADRJOKAFUK1'
+global.fglog = 'https://telegra.ph/file/7721cf222f9790f3fcd1b.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
